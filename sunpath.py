3dcd3eff6ca7ca3baa13e3d1e09b242f3af28f44
Pedro Damas <pedrodamas1@gmail.com>
2024-04-30 00:07:42 +0100

    Updates

---
+++ sunpath.py
@@ -0,0 +1,69 @@
+# Data handling and processing
+import pandas as pd
+import numpy as np
+
+# Solar position calculations
+from pvlib import solarposition
+
+# Display
+import plotly.express as px
+import plotly.graph_objects as go
+from IPython.display import display, Image
+import matplotlib.pyplot as plt
+import matplotlib.tri as tri
+import matplotlib.dates as mdates
+import seaborn as sns
+
+# Define the start and end dates, considering a whole year
+start = f'2024-01-01 00:00:00'
+end = f'2024-12-31 23:59:00'
+
+# Generate a sequence of datetimes in 15 minutes intervals
+times = pd.date_range(start, end, freq='15min')
+
+# Calculate solar position for the daterange generated (coordinate for Peniche) 
+df = solarposition.get_solarposition(times, latitude=39.34883424, longitude=-9.34298515)
+
+# Calculate the cartesian angle
+df['theta'] = 90 - df['azimuth']
+
+# It is easy to extract datetime information
+df['month'] = df.index.month
+df['day'] = df.index.day
+df['time'] = df.index.time
+df['minute'] = df.index.minute
+df['hour'] = df.index.hour
+df['dayofyear'] = df.index.dayofyear
+df['date'] = pd.to_datetime(df.index.date)
+
+# Setup the solar path dome coordinates
+RADIUS = 1
+df['x'] = RADIUS * np.sin(df['apparent_zenith']*np.pi/180) * np.cos(df['theta']*np.pi/180)
+df['y'] = RADIUS * np.sin(df['apparent_zenith']*np.pi/180) * np.sin(df['theta']*np.pi/180)
+df['z'] = RADIUS * np.cos(df['apparent_zenith']*np.pi/180)
+
+# Setup the dome animation
+
+# Simulation 1
+# fig = px.line_3d(df, x='x', y='y', z='z', hover_name='time', animation_frame='dayofyear')
+
+# Simulation 2
+# fig = px.line_3d(df[df['day']==21], x='x', y='y', z='z', hover_name='time', color='month')
+
+# Simulation 3
+fig = px.line_3d(df[df['day']==21], x='x', y='y', z='z', color='month')
+fig2 = px.line_3d(df[df['minute']==0], x='x', y='y', z='z', color='time')
+fig.add_traces(list(fig2.select_traces()))
+
+# 
+
+fig.update_layout(
+    scene = dict(
+        xaxis = dict(range=[-2,2],),
+        yaxis = dict(range=[-2,2],),
+        zaxis = dict(range=[-2,2],),
+        aspectratio=dict(x=1, y=1, z=1)
+    ),
+)
+fig.show()
+
