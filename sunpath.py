b37443498e5cf6d5454c75a4bee9a4d63a567f78
Pedro Damas <pedrodamas1@gmail.com>
2024-04-30 09:18:02 +0100

    Updates

--- sunpath.py
+++ sunpath.py
@@ -16,7 +16,7 @@ import seaborn as sns
 
 # Define the start and end dates, considering a whole year
 start = f'2024-01-01 00:00:00'
-end = f'2024-12-31 23:59:00'
+end = f'2024-12-31 23:59:59'
 
 # Generate a sequence of datetimes in 15 minutes intervals
 times = pd.date_range(start, end, freq='15min')
@@ -29,6 +29,7 @@ df['theta'] = 90 - df['azimuth']
 
 # It is easy to extract datetime information
 df['month'] = df.index.month
+df['month_name'] = df.index.month_name()
 df['day'] = df.index.day
 df['time'] = df.index.time
 df['minute'] = df.index.minute
@@ -51,19 +52,27 @@ df['z'] = RADIUS * np.cos(df['apparent_zenith']*np.pi/180)
 # fig = px.line_3d(df[df['day']==21], x='x', y='y', z='z', hover_name='time', color='month')
 
 # Simulation 3
-fig = px.line_3d(df[df['day']==21], x='x', y='y', z='z', color='month')
-fig2 = px.line_3d(df[df['minute']==0], x='x', y='y', z='z', color='time')
-fig.add_traces(list(fig2.select_traces()))
+# fig = px.line_3d(df[df['day']==21], x='x', y='y', z='z', color='month')
+# fig2 = px.line_3d(df[df['minute']==0], x='x', y='y', z='z', color='time')
+# fig.add_traces(list(fig2.select_traces()))
 
-# 
+# fig.update_layout(
+#     scene = dict(
+#         xaxis = dict(range=[-2,2],),
+#         yaxis = dict(range=[-2,2],),
+#         zaxis = dict(range=[-2,2],),
+#         aspectratio=dict(x=1, y=1, z=1)
+#     ),
+# )
+# fig.show()
 
-fig.update_layout(
-    scene = dict(
-        xaxis = dict(range=[-2,2],),
-        yaxis = dict(range=[-2,2],),
-        zaxis = dict(range=[-2,2],),
-        aspectratio=dict(x=1, y=1, z=1)
-    ),
-)
-fig.show()
+fig, ax = plt.subplots()
+
+sns.lineplot(data=df[(df['day']==21) & (df['month'].isin([1,2,3,4,5,6]))], x="azimuth", y="apparent_elevation", hue="month_name", color='red', ax=ax)
+sns.lineplot(data=df[df['minute']==0], x="azimuth", y="apparent_elevation", hue="time", color='blue', sort=False, ax=ax)
+ax.get_legend().remove()
+plt.grid()
+plt.show()
+
+
 
